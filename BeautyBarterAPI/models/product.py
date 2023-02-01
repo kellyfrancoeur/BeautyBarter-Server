f908cb00f803217b45b47f3b02bb3c638217cca5
Kelly Francoeur <kellyafrancoeur@gmail.com>
2023-01-31 20:03:01 -0600

    Adjusted product model

--- BeautyBarterAPI/models/product.py
+++ BeautyBarterAPI/models/product.py
@@ -1,19 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
 
-    member = models.ForeignKey("Member", null=True, blank=True, on_delete=models.CASCADE)
-    admin = models.ForeignKey("Admin", null=True, blank=True, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     product_category = models.ForeignKey("ProductType", null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     instructions = models.TextField(blank=True)
     cost = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.member.username
-
-    def __str__(self):
-        return self.admin.username
+        return self.added_by.username
 
     def __str__(self):
         return self.product_category.category
